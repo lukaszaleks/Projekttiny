03dcc28d182dbd9ca6d8ff4a15234a210375a1e8
lukaszaleks <115348192+lukaszaleks@users.noreply.github.com>
2024-01-15 09:56:05 +0100

    Add files via upload

---
+++ scripts/messegeDialogWindow.js
@@ -0,0 +1,77 @@
+
+
+const buttonToOpen = document.querySelector('.messageButton'); 
+
+
+
+const hideForm = () => { 
+  document.querySelector('.messageInfo').classList.add('hide'); 
+  document.querySelector('.messageInfo-container').classList.add('hide'); 
+  document.querySelector('.messageInfo').addEventListener('transitionend', ()=>{
+    document.querySelectorAll('.blurWhenMessegeActive').forEach(element => element.classList.remove('blur')) 
+    document.querySelector('.messageInfo-container').remove(); 
+    buttonToOpen.classList.remove('hide'); 
+    document.querySelector('.arrowUp').classList.remove('hide'); 
+  })
+}
+
+
+const showForm = () => {
+  const messageBody = `
+    <div class="messageInfo hide">
+      <div class="closeButton">
+        <img src="images/close.png" alt="closeButton">
+      </div>
+      <p class='mail'>lukasz.aleksandrowicz11@gmail.com</p>
+      <div class="images">
+        <a href="https://www.linkedin.com/in/%C5%82ukasz-aleksandrowicz-08926b245/">
+          <img src="images/linkedin.png" alt="linkedin">
+          <p>linkedin</p>
+        </a>
+      </div>
+    </div>`; 
+
+  buttonToOpen.classList.add('hide'); 
+  document.querySelector('.arrowUp').classList.add('hide'); 
+  
+  let isTransitionEndHandled = false; 
+ 
+
+  buttonToOpen.addEventListener('transitionend', event => {
+    if (!isTransitionEndHandled && event.propertyName === 'opacity') { 
+      
+
+      const createWindow = document.createElement('div'); 
+      document.querySelector('body').prepend(createWindow) 
+      createWindow.classList.add('messageInfo-container'); 
+      createWindow.innerHTML = messageBody; 
+    
+      setTimeout(()=>document.querySelector('.messageInfo').classList.remove('hide'));
+      
+
+      document.querySelector('.messageInfo .closeButton').addEventListener('click', ()=> hideForm())
+     
+      
+      isTransitionEndHandled = true;
+     
+    }
+  })
+}
+
+
+const blurScreen = () => {
+  document.querySelectorAll('.blurWhenMessegeActive').forEach(element => {
+    element.classList.toggle('blur');
+  })
+}
+
+
+buttonToOpen.addEventListener('click',()=> {
+  blurScreen();
+  showForm();
+})
+
+
+
+
+
---
+++ scripts/scrollUp.js
@@ -0,0 +1,23 @@
+
+
+const scrollUp = () => {
+    window.scrollTo({
+        top:0,
+        left:0,
+        behavior:'smooth' 
+    })
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    scrollUp()
+});
+
+
+document.querySelector('.arrowUp').addEventListener('click', ()=> scrollUp())
+
+
+document.addEventListener('scroll', () => {
+    if(window.scrollY >= 300){
+        document.querySelector('.arrowUp').classList.add('showWhenScroll');
+    } else document.querySelector('.arrowUp').classList.remove('showWhenScroll');
+})
